74edb324073c8a63ac130320b0ec58b987a2383f
Hung <nguyenhungdev.fe@gmail.com>
2023-04-05 01:14:35 +0700

    update

--- video4/video4.js
+++ video4/video4.js
@@ -3,5 +3,11 @@
 let a = 'Tôi là một string - chuỗi';
 
 console.log(a);
-console.log(4 + 5 + 'a');
-console.log('a' + 5 + 4);
+console.log(4 + 5 + 'a'); // = 9 + 'a' = 9a;
+console.log('a' + 5 + 4); // a5 + 4 = a54; 
+/*
+Note:
+    + Cộng số với số thì thành số
+    + Số cộng chuỗi thì thành chuỗi
+    + Cộng từ trái sang phải
+*/
