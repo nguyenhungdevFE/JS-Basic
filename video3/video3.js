40f0e87301bdfea5685cdd2f24e853ab5f7766f6
Hung <nguyenhungdev.fe@gmail.com>
2023-04-05 00:50:59 +0700

    update

--- video3/video3.js
+++ video3/video3.js
@@ -1,3 +1,8 @@
-console.log('Hello world from javascript');
 
+// const dùng để khi bạn muốn cố định 1 giá trị
+console.log('Hello world from javascript',);
+
+
+let x = 5, y = 6, z = 7;
+console.log('Teststring', x, y, z);
 // `` quotes use to read all contains single quotes and double quotes
---
+++ video4/video4.js
@@ -0,0 +1,3 @@
+let x = 5, y = 6, z = 7;
+console.log("TestString",'x=', x,'y= ', y,'z=', z); // code không lỗi
+console.log("TestString", + 'x=' + x + 'y= '+ y + 'z=' + z);
