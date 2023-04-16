82be29eee21f6a9b55328c1a7ad457b9070689da
Hung <nguyenhungdev.fe@gmail.com>
2023-04-17 00:26:46 +0700

    update

--- video17/video17.js
+++ video17/video17.js
@@ -1,6 +1,10 @@
 console.log('Hello world from HTML');
 
 
-function getName (x, y) {
-    
+function sum (x, y) {
+    x + y; // undefined bởi vì hàm không trả về giá trị gì
+    return x + y; // hàm trả vè x + y 
 }
+
+
+console.log('sum = ', sum(6,9));
---
+++ video18/video18.js
@@ -0,0 +1,13 @@
+//                      ARROW FOUCTION
+
+
+
+// Ký hiệu là => { ................ }
+
+// examples 
+let sum = (a,b) =>{
+    return a + b;
+}
+
+
+console.log('check sum', sum(6,9));
