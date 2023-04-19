0c339c52598d42f5cc81138985ce5e0a215c4e3c
Hung <nguyenhungdev.fe@gmail.com>
2023-04-19 14:58:21 +0700

    update

--- video22/video22.js
+++ video22/video22.js
@@ -2,4 +2,19 @@ console.log('Hello World from HTMl');
 
 //                  MAP - will be used in React
 
+let arr = [1,2,3,4,5,6,7,8,9,10];
+// for(let i = 0; i < arr.length; i++){
+//     arr[i] = arr[i] * arr[i];
+// }
+
+console.log('>>> check value i:', arr);
+
+
+
+let mapArr = arr.map((item,index) => {
+    item = item * item;
+    return item;
+})
+
+console.log('>>> check value mapArr: ', mapArr);
 
